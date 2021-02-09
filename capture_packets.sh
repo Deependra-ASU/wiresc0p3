f2e4a9d76c280af6afef5dfc281d4e88ecdc4dfd
Robert McGuigan <rdmcguig@asu.edu>
2021-02-08 20:30:26 -0800

    Basic Logging Capability
    Added capability to read incoming pcap files, process them and log
    processed data to /logs/service<portnum>.  Deletes raw pcap file after
    analysis.

--- capture_packets.sh
+++ capture_packets.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 readonly HERE=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-sudo tcpdump -i en0 -s 65535 -w ${HERE}/analyze/output.pcap -C 1
+sudo tcpdump -s 65535 -w ${HERE}/analyze/output.pcap -C 1 -W 2 -Z root
--- test.py
+++ test.py
@@ -1,20 +1,70 @@
-import codecs
+import types, codecs, os, time, sys
 
 from scapy.all import *
 from scapy.layers.http import HTTP
 from scapy.layers.inet import IP, TCP, UDP
 from scapy.layers.l2 import Loopback
 
-pcap_files = []
-for (dir_path, dir_names, file_names) in os.walk('analyze'):
-    pcap_files.extend(file_names)
+def get_file_list(port_nums):
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+    file_list = [];
+    for port_num in port_nums:
+        log_file = open("logs/service" + str(port_num), "a+")
+        file_list.append(log_file)
+    return file_list
 
-for pcap_file in pcap_files:
-    print(f'analyzing file ./analyze/{pcap_file}...\n')
-    pkts = rdpcap(f'analyze/{pcap_file}')
+def analyze_pcap(filename, port_nums, log_files):
+    print('analyzing file ' + filename + '\n')
+    pkts = rdpcap(filename)
+    print(len(pkts))
 
     for pkt in pkts:
         # if Raw in pkt:
         #     raw = pkt[Raw]
         #     hexdump(raw)
-        print(pkt.layers())
+        # TODO: if TCP, get portnum and match with log_file, get payload,
+        # run against validator script, if passes write to log file
+
+        # print(pkt.layers())
+        layers = pkt.layers()
+        log_files[0].write(str(layers) + '\n')
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: ./test.py [<portnum>, <portnum>, ...]")
+        exit()
+
+    port_nums = map(int, sys.argv[1].strip('[]').split(','))
+    print(port_nums)
+
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+    log_files = [];
+    for port_num in port_nums:
+        log_file = open("logs/service" + str(port_num), "w+")
+        log_files.append(log_file)
+    
+    pcap_filename = "analyze/output.pcap0"
+    next_pcap_filename = "analyze/output.pcap1"
+    while True:
+        try:
+            next_file = open(next_pcap_filename, "r")
+            try:
+                current_file = open(pcap_filename, "r")
+                try:
+                    analyze_pcap(pcap_filename, port_nums, log_files)
+                except (Scapy_Exception):
+                    print("Couldn't read packets from " + pcap_filename + " (may be empty)")
+                current_file.close()
+                os.remove(pcap_filename)
+                temp = pcap_filename
+                pcap_filename = next_pcap_filename
+                next_pcap_filename = temp
+            except (FileNotFoundError, IOError):
+                print("Expected log file not found, skipping")
+        except (FileNotFoundError, IOError):
+            print("couldn't open file")
+            time.sleep(1)
+
+main()
