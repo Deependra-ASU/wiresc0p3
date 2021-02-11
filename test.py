1a609d6d62ef88c780933d9533023706822499e4
Robert McGuigan <rdmcguig@asu.edu>
2021-02-10 21:18:16 -0800

    Time-Based pcap Processing
    Changed processing of pcap files to occur every 15 seconds instead of
    based on size, since 1MB was smallest supported size by tcpdump.  Raw
    logs are moved to archive/ post processing and are deleted after 2
    minutes.

--- test.py
+++ test.py
@@ -1,23 +1,24 @@
-import types, codecs, os, time, sys
+import types, codecs, os, time, sys, datetime
 
 from scapy.all import *
 from scapy.layers.http import HTTP
 from scapy.layers.inet import IP, TCP, UDP
 from scapy.layers.l2 import Loopback
 
-def get_file_list(port_nums):
-    if not os.path.exists('logs'):
-        os.makedirs('logs')
-    file_list = [];
-    for port_num in port_nums:
-        log_file = open("logs/service" + str(port_num), "a+")
-        file_list.append(log_file)
-    return file_list
+def get_epoch_seconds(filename):
+    print("getting seconds from " + filename)
+    seconds = None
+    try:
+        filename_list = filename.split('-')
+        seconds = int(filename_list[1])
+    except(IndexError, ValueError):
+        print("Failed to parse filename" + filename)
+    return seconds
 
 def analyze_pcap(filename, port_nums, log_files):
     print('analyzing file ' + filename + '\n')
-    pkts = rdpcap(filename)
-    print(len(pkts))
+#    try:
+    pkts = rdpcap('analyze/' + filename)
 
     for pkt in pkts:
         # if Raw in pkt:
@@ -25,46 +26,44 @@ def analyze_pcap(filename, port_nums, log_files):
         #     hexdump(raw)
         # TODO: if TCP, get portnum and match with log_file, get payload,
         # run against validator script, if passes write to log file
-
+        
         # print(pkt.layers())
         layers = pkt.layers()
         log_files[0].write(str(layers) + '\n')
+    os.rename('analyze/' + filename, 'archive/' + filename)
+#    except(FileNotFoundError, IOError, Scapy_Exception):
+#        print("Failed to process pcap file (might be empty)")
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: ./test.py [<portnum>, <portnum>, ...]")
+        print("Usage: ./test.py [<portnum>,<portnum>,...]")
         exit()
 
     port_nums = map(int, sys.argv[1].strip('[]').split(','))
-    print(port_nums)
 
     if not os.path.exists('logs'):
         os.makedirs('logs')
+    if not os.path.exists('archive'):
+        os.makedirs('archive')
+    
     log_files = [];
     for port_num in port_nums:
         log_file = open("logs/service" + str(port_num), "w+")
         log_files.append(log_file)
     
-    pcap_filename = "analyze/output.pcap0"
-    next_pcap_filename = "analyze/output.pcap1"
     while True:
-        try:
-            next_file = open(next_pcap_filename, "r")
-            try:
-                current_file = open(pcap_filename, "r")
-                try:
-                    analyze_pcap(pcap_filename, port_nums, log_files)
-                except (Scapy_Exception):
-                    print("Couldn't read packets from " + pcap_filename + " (may be empty)")
-                current_file.close()
-                os.remove(pcap_filename)
-                temp = pcap_filename
-                pcap_filename = next_pcap_filename
-                next_pcap_filename = temp
-            except (FileNotFoundError, IOError):
-                print("Expected log file not found, skipping")
-        except (FileNotFoundError, IOError):
-            print("couldn't open file")
-            time.sleep(1)
+        print("new iteration")
+        current_time = time.time()
+        for filename in os.listdir('analyze'):
+            ctime = get_epoch_seconds(filename)
+            print("deltatime")
+            print(current_time - ctime)
+            if current_time - ctime > 30:
+                analyze_pcap(filename, port_nums, log_files)
+        for filename in os.listdir('archive'):
+            ctime = get_epoch_seconds(filename)
+            if ctime - current_time > 120:
+                os.remove(filename)
+        time.sleep(15)
 
 main()
