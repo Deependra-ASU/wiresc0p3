d6ec594a13df7ac82231ec899710dac302741e07
Robert McGuigan <rdmcguig@asu.edu>
2021-02-19 14:16:46 -0800

    Reorganized file structure, fixed Makefile
    Added directory structure to project, fixed typos in Makefile, adjusted
    deploy script and Makefile for directory structure

--- Makefile
+++
@@ -1,9 +0,0 @@
-all: portscan.c
-	gcc -o portscan portscan.c
-	mkdir -p out/tcpdump
-	mkdir -p out/tcpflow
-	chmod u+x checkfile.py
-	chmod u+x honeypot.py
-	chmod u+x mongo_connect.py
-	chmod u+x process_tcpdump.py
-	chmod u+x process_tcpflow.py
--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 if [[ "$#" -ne 3 ]]
 then
-  echo "Usage: ./deploy.sh <path/to/sshkey> <path/to/local/wiresc0p3> <ctf@IP:/path/to/dst>"
+  echo "Usage: ./deploy.sh <path/to/sshkey> <path/to/local/wiresc0p3/src> <ctf@IP:/path/to/dst>"
 else
   rsync -r -e "ssh -i $1" $2 $3
 fi
